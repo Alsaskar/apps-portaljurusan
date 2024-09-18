8f691446ab2ad855132bd0f6ed6e55b542745c11
Oswald2809 <oswaldtanlee44@gmail.com>
2024-09-18 12:03:33 +0800

    Perubahan pada route

--- backend/src/routes/RPSRoute.js
+++ backend/src/routes/RPSRoute.js
@@ -2,21 +2,34 @@ import express from "express";
 import CekToken from "../middleware/CekToken";
 import {
   add,
+  addDetail,
+  approve,
   detail,
+  detailMingguan,
   edit,
+  editDetail,
   list,
-  remove
+  reject,
+  remove,
+  removeDetail
 } from "../controllers/RPSController";
 
 const router = express.Router();
 
 router.post("/", CekToken, add);
+router.post("/mingguan", CekToken, addDetail);
 
 router.get("/", CekToken, list);
 router.get("/:id", CekToken, detail);
+router.get("/mingguan/:idRps", CekToken, detailMingguan);
 
 router.put("/:id", CekToken, edit);
+router.put("/mingguan/:id", CekToken, editDetail);
+
+router.put("/approve/:id", CekToken, approve);
+router.put("/reject/:id", CekToken, reject);
 
 router.delete("/:id", CekToken, remove);
+router.delete("/mingguan/:id", CekToken, removeDetail);
 
 export default router;
