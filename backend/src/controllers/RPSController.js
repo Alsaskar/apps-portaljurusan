6092f48af7c114f7d3f9032789f8dcd5e34d7da0
Oswald2809 <oswaldtanlee44@gmail.com>
2024-09-18 20:30:12 +0800

    Update RPS

--- backend/src/controllers/RPSController.js
+++ backend/src/controllers/RPSController.js
@@ -1,6 +1,70 @@
 import Matkul from "../models/MatkulModels";
 import { DetailRPS, RPS } from "../models/RPSModel";
 
+// export const add = async (req, res) => {
+//     const idDosen = req.body.idDosen;
+//     const kodeMatkul = req.body.kodeMatkul;
+//     const rumpunMatkul = req.body.rumpunMatkul;
+//     const bobot = req.body.bobot;
+//     const semester = req.body.semester;
+//     const tanggalPenyusunan = req.body.tanggalPenyusunan;
+//     const otorisasi = req.body.otorisasi;
+//     const pembuatRp = req.body.pembuatRp;
+//     const pengampuMatkul = req.body.pengampuMatkul;
+//     const kordinatorMatkul = req.body.kordinatorMatkul;
+//     const kordinatorProdi = req.body.kordinatorProdi;
+//     const capaianPembelajaran = req.body.capaianPembelajaran;
+//     const cpl = req.body.cpl;
+//     const cpmk = req.body.cpmk;
+//     const subCpmk = req.body.subCpmk;
+//     const deskripsiMk = req.body.deskripsiMk;
+//     const bahanKajian = req.body.bahanKajian;
+//     const daftarPustaka = req.body.daftarPustaka;
+//     const dosenPengampu = req.body.dosenPengampu;
+//     const matkulPrasyarat = req.body.matkulPrasyarat;
+
+//     // get nama matkul berdasarkan kode matkul
+//     const matkul = await Matkul.findOne({ where: { kodeMatkul: kodeMatkul }, attributes: ['matkul', 'kodeMatkul'] })
+//     const namaMatkul = matkul.dataValues.matkul;
+
+//     if(kodeMatkul === matkul.dataValues.kodeMatkul){ // jika matkul sudah ada rps, maka tidak bisa
+//         return res.status(500).json({ message: 'RPS pada Matkul yang Anda pilih sudah ada sebelumnya', success: false })
+//     }else{
+//         try{
+
+//             await RPS.create({
+//                 idDosen: idDosen,
+//                 namaMatkul: namaMatkul,
+//                 kodeMatkul: kodeMatkul,
+//                 rumpunMatkul: rumpunMatkul,
+//                 bobot: bobot,
+//                 semester: semester,
+//                 tanggalPenyusunan: tanggalPenyusunan,
+//                 otorisasi: otorisasi,
+//                 pembuatRp: pembuatRp,
+//                 pengampuMatkul: pengampuMatkul,
+//                 kordinatorMatkul: kordinatorMatkul,
+//                 kordinatorProdi: kordinatorProdi,
+//                 capaianPembelajaran: capaianPembelajaran,
+//                 cpl: cpl,
+//                 cpmk: cpmk,
+//                 subCpmk: subCpmk,
+//                 deskripsiMk: deskripsiMk,
+//                 bahanKajian: bahanKajian,
+//                 daftarPustaka: daftarPustaka,
+//                 dosenPengampu: dosenPengampu,
+//                 matkulPrasyarat: matkulPrasyarat,
+//             })
+    
+//             return res.status(200).json({ message: 'Berhasil menambahkan RPS', success: true })
+//         }catch(err){
+//             return res.status(500).json({ message: err })
+//         }
+//     }
+// }
+
+
+
 export const add = async (req, res) => {
     const idDosen = req.body.idDosen;
     const kodeMatkul = req.body.kodeMatkul;
@@ -23,45 +87,52 @@ export const add = async (req, res) => {
     const dosenPengampu = req.body.dosenPengampu;
     const matkulPrasyarat = req.body.matkulPrasyarat;
 
-    // get nama matkul berdasarkan kode matkul
-    const matkul = await Matkul.findOne({ where: { kodeMatkul: kodeMatkul }, attributes: ['matkul', 'kodeMatkul'] })
-    const namaMatkul = matkul.dataValues.matkul;
-
-    if(kodeMatkul === matkul.dataValues.kodeMatkul){ // jika matkul sudah ada rps, maka tidak bisa
-        return res.status(500).json({ message: 'RPS pada Matkul yang Anda pilih sudah ada sebelumnya', success: false })
-    }else{
-        try{
-
-            await RPS.create({
-                idDosen: idDosen,
-                namaMatkul: namaMatkul,
-                kodeMatkul: kodeMatkul,
-                rumpunMatkul: rumpunMatkul,
-                bobot: bobot,
-                semester: semester,
-                tanggalPenyusunan: tanggalPenyusunan,
-                otorisasi: otorisasi,
-                pembuatRp: pembuatRp,
-                pengampuMatkul: pengampuMatkul,
-                kordinatorMatkul: kordinatorMatkul,
-                kordinatorProdi: kordinatorProdi,
-                capaianPembelajaran: capaianPembelajaran,
-                cpl: cpl,
-                cpmk: cpmk,
-                subCpmk: subCpmk,
-                deskripsiMk: deskripsiMk,
-                bahanKajian: bahanKajian,
-                daftarPustaka: daftarPustaka,
-                dosenPengampu: dosenPengampu,
-                matkulPrasyarat: matkulPrasyarat,
-            })
-    
-            return res.status(200).json({ message: 'Berhasil menambahkan RPS', success: true })
-        }catch(err){
-            return res.status(500).json({ message: err })
+    try {
+        // Cek apakah sudah ada RPS untuk kodeMatkul yang sama
+        const existingRps = await RPS.findOne({ where: { kodeMatkul } });
+        if (existingRps) {
+            return res.status(400).json({ message: 'RPS pada Matkul yang Anda pilih sudah ada sebelumnya', success: false });
         }
+
+        // Get nama matkul berdasarkan kode matkul
+        const matkul = await Matkul.findOne({ where: { kodeMatkul }, attributes: ['matkul', 'kodeMatkul'] });
+        if (!matkul) {
+            return res.status(404).json({ message: 'Mata Kuliah tidak ditemukan', success: false });
+        }
+
+        const namaMatkul = matkul.dataValues.matkul;
+
+        // Create RPS baru
+        await RPS.create({
+            idDosen: idDosen,
+            namaMatkul: namaMatkul,
+            kodeMatkul: kodeMatkul,
+            rumpunMatkul: rumpunMatkul,
+            bobot: bobot,
+            semester: semester,
+            tanggalPenyusunan: tanggalPenyusunan,
+            otorisasi: otorisasi,
+            pembuatRp: pembuatRp,
+            pengampuMatkul: pengampuMatkul,
+            kordinatorMatkul: kordinatorMatkul,
+            kordinatorProdi: kordinatorProdi,
+            capaianPembelajaran: capaianPembelajaran,
+            cpl: cpl,
+            cpmk: cpmk,
+            subCpmk: subCpmk,
+            deskripsiMk: deskripsiMk,
+            bahanKajian: bahanKajian,
+            daftarPustaka: daftarPustaka,
+            dosenPengampu: dosenPengampu,
+            matkulPrasyarat: matkulPrasyarat,
+        });
+
+        return res.status(200).json({ message: 'Berhasil menambahkan RPS', success: true });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
     }
-}
+};
+
 
 export const list = async (req, res) => {
     try{
